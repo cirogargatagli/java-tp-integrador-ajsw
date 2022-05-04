fb03df7def710ee10907358adfc42af0eeeee36a
cgargatagli <ciroshaila@gmail.com>
2022-05-03 23:04:36 -0300

    First commit

---
+++ src/main/java/com/ajsw/javausersservice/JavaUsersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ajsw.javausersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUsersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaUsersServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ajsw/javausersservice/JavaUsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajsw.javausersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUsersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
